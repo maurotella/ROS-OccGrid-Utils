717b49122453072404e63f1e2fcf82bdbcde9b44
Matteo Princisgh <mauro.tellaroli@studenti.unimi.it>
2024-04-08 12:55:08 +0200

    added merge_maps

--- OccupancyGridUtils.py
+++ OccupancyGridUtils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from math import inf
 import matplotlib.pyplot as plt
 from matplotlib.axes._axes import Axes
 from matplotlib.colors import LinearSegmentedColormap
@@ -21,7 +22,7 @@ def get_map_img(map_data:list, size:tuple, crop=False, padding=30):
     map_data: list[int]
         The data field of nav_msgs/OccupancyGrid.msg
     size: tuple[int, int]
-        Tuple containing the height and the width of the map
+        Tuple containing height and width of the map
     cut: bool
         If True the map will be cropped removing the unscanned (-1) cells
         in the map outline
@@ -61,7 +62,6 @@ def get_map_img(map_data:list, size:tuple, crop=False, padding=30):
         cut_img = img
     return cut_img
 
-
 def plot_map_img(img:list, ax:Axes, bg_color="#607372") -> None:
     '''
     Plots the map *img* into the matplotlib ax *ax*
@@ -73,7 +73,7 @@ def plot_map_img(img:list, ax:Axes, bg_color="#607372") -> None:
     ax: Axes
         matplotlib ax in which the map will be plotted
     bg_color: str
-        Color of the unscanned cells (-1) pf the map; the format is the
+        Color of the unscanned cells (-1) of the map; the format is the
         same of matplotlib. The default color is the color used by RViz
     '''
     cmap = LinearSegmentedColormap.from_list('my_gradient', (
@@ -87,5 +87,67 @@ def plot_map_img(img:list, ax:Axes, bg_color="#607372") -> None:
     masked = np.ma.masked_where(img==-1,img)
     ax.imshow(masked,cmap=cmap,origin='lower')
 
-if __name__=='__main__':
-    plot_map_img(get_map_img(MAPS['marty']),plt.gca())
+def merge_maps(maps_data:list, maps_resolution:list, maps_origin:list, maps_size:list, threshold=70):
+    '''
+    Returns the map resulting from the merging of the maps *maps_data*.
+
+    Parameters
+    ----------
+    maps_data: list[list[int]]
+        Ordered list of data fields of nav_msgs/OccupancyGrid.msg
+    maps_resolution: list[float]
+        Ordered list of map resolutions
+    maps_origin: list[tuple[float,float]]
+        Ordered list of map origins; each origin is a tuple containing its x,y
+    maps_size: list[tuple[int,int]]
+        Ordered list of map sizes; each size is a tuple containing its height and width
+    threshold: int
+        Threshold above which a cell is considered an obstacle
+    
+    Returns
+    -------
+    merged_map: list[list[int]]
+        The map resulting from the merging of the maps *maps_data*. The merging
+        is an overlapping of the maps based on their origins.
+    '''
+    grid = {}
+    min_x = inf 
+    min_y = inf
+    max_x = -inf
+    max_y = -inf
+    for index in range(len(maps_data)):
+        occupancy = maps_data[index]
+        resolution = maps_resolution[index]
+        origin_x, origin_y = maps_origin[index]
+        height, width = maps_size[index]
+        for i in range(width):
+            for j in range(height):
+                if occupancy[i+j * width] == -1:
+                    continue
+                if occupancy[i+j * width] > threshold:
+                    x = (i * resolution) + origin_x
+                    y = (j * resolution) + origin_y
+                    p = (x, y)
+                    grid[p] = True
+                elif occupancy[i+j * width] <= threshold:
+                    x = (i * resolution) + origin_x
+                    y = (j * resolution) + origin_y
+                    p = (x, y)
+                    if p not in grid:
+                        grid[p] = False
+                if x<min_x:
+                    min_x=x
+                elif x>max_x:
+                    max_x=x
+                if y<min_y:
+                    min_y=y
+                elif y>max_y:
+                    max_y=y
+    h,w = round(abs(max_y-min_y)/resolution),round(abs(max_x-min_x)/resolution)
+    o_x,o_y = min_x,min_y
+    img = np.zeros((h+1,w+1))-1
+    for p in grid:
+        x,y = p
+        x,y = round((x-o_x)/resolution),round((y-o_y)/resolution)
+        img[y,x] = 100 if grid[p] else 0
+    return img
